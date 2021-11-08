e52114dc2ff50a3013fc3e1ae8079f06dfeaacd3
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-11-08 16:01:48 -0600

    chpt 12 complete

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,26 +1,78 @@
-import React, { useState, useEffect } from "react"
-import { getEvents } from "./EventManager.js"
-import { useHistory } from 'react-router-dom'
+// import React, { useState, useEffect } from "react"
+// import { getEvents } from "./EventManager.js"
+// import { useHistory } from 'react-router-dom'
 
-export const EventList = (props) => {
-    const [ events, setEvents ] = useState([])
+// export const EventList = (props) => {
+//     const [ events, setEvents ] = useState([])
+//     const history = useHistory()
+
+//     useEffect(() => {
+//         getEvents().then(data => setEvents(data))
+//     }, [])
+
+//     return (
+//         <article className="events">
+//             <button className="btn btn-2 btn-sep icon-create"
+//                 onClick={() => {
+//                     history.push({ pathname: "/events/new" })
+//                 }}
+//             >Register New Event</button>
+//             {
+//                 events.map(event => {
+//                     return <section key={`event--${event.id}`} className="event">
+//                         <div className="event__description">{event.description} by {event.organizer.user.first_name}</div>
+//                     </section>
+//                 })
+//             }
+//         </article>
+//     )
+// }
+
+import React, { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { getEvents, joinEvent, leaveEvent } from "./EventManager.js"
+
+
+export const EventList = () => {
     const history = useHistory()
+    const [ events, assignEvents ] = useState([])
+
+    const eventFetcher = () => {
+        getEvents()
+            .then(data => assignEvents(data))
+    }
 
     useEffect(() => {
-        getEvents().then(data => setEvents(data))
+        eventFetcher()
     }, [])
 
     return (
         <article className="events">
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    history.push({ pathname: "/events/new" })
-                }}
-            >Register New Event</button>
+            <header className="events__header">
+                <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
+            </header>
             {
                 events.map(event => {
-                    return <section key={`event--${event.id}`} className="event">
-                        <div className="event__description">{event.description} by {event.organizer.user.first_name}</div>
+                    return <section key={event.id} className="registration">
+                        <div className="registration__game">{event.game.title}</div>
+                        <div>{event.description}</div>
+                        <div>
+                            {event.date} @ {event.time}
+                        </div>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id).then(() => eventFetcher())}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id).then(() => eventFetcher())}
+                                    >Join</button>
+                        }
                     </section>
                 })
             }
--- src/components/event/EventManager.js
+++ src/components/event/EventManager.js
@@ -18,3 +18,25 @@ export const createEvent = (newEvent) => {
     })
         .then(res => res.json())
 }
+
+export const joinEvent = (eventId) => {
+    return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+        method: "POST",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+        //.then(getEvents)
+}
+
+export const leaveEvent = (eventId) => {
+    return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+        method: "DELETE",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        // .then(response => response.json())
+        .then(getEvents)
+}
